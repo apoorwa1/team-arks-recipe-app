58420bdb9e51eeadc90e1ccece01e5d9977b5eb7
Apoorwa <apoorwa.saxena@gmail.com>
2022-05-20 11:13:30 +0100

    recipe component WIP

--- arks-recipe-app/src/App.js
+++ arks-recipe-app/src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import RecipeSearch from "./RecipeSearch";
 
 function App() {
   return (
     <div className="App">
       <h1>Food Finder</h1>
+      <RecipeSearch />
     </div>
   );
 }
